4be7b4a48eee859859b941d364605af3d281914e
TheComet <alex.murray@gmx.ch>
2014-02-02 01:59:19 +0000

    fixed SDL build for windows

--- build-nmake.bat
+++ build-nmake.bat
@@ -1,7 +1,8 @@
+@echo off
+call vcvars32.bat x86
+
 REM build project
 mkdir "build.dragon.nmake.debug"
 cd "build.dragon.nmake.debug"
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
-
-exit 0
-
+cd ..
